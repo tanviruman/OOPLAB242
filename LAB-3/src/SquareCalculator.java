b6332883ceaaaab20893eeb2346909f96eb9c62b
tanviruman <ruman777ahmed@gmail.com>
2024-06-23 08:46:02 +0600

    Refactor SquareCalculator class to improve user input handling and code readability

--- LAB-3/src/SquareCalculator.java
+++ LAB-3/src/SquareCalculator.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SquareCalculator {
+import java.util.Scanner;
+
+public class SquareCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Please enter a number: ");
+
+
+        double number = scanner.nextDouble();
+
+
+        double square = number * number;
+
+
+        System.out.println("The square of " + number + " is: " + square);
+
+
+        scanner.close();
+    }
 }
