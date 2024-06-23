96a38c111e261074b2c24031210dd9a52206736b
tanviruman <ruman777ahmed@gmail.com>
2024-06-23 08:51:01 +0600

    Refactor SumCalculator class to improve user input handling and code readability

--- LAB-3/src/SumCalculator.java
+++ LAB-3/src/SumCalculator.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class SumCalculator {
+import java.util.Scanner;
+
+public class SumCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Please enter the first number: ");
+
+        double number1 = scanner.nextDouble();
+
+
+        System.out.print("Please enter the second number: ");
+
+        double number2 = scanner.nextDouble();
+
+
+        double sum = number1 + number2;
+
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+
+        scanner.close();
+    }
 }
