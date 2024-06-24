b520b22958f024680abb619ee51dfecb401a0115
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-06-24 19:44:15 +0600

    Update AddNumbers.java

--- LAB-1/javatpointlab1/src/AddNumbers.java
+++ LAB-1/javatpointlab1/src/AddNumbers.java
@@ -2,7 +2,7 @@ public class AddNumbers {
 
     public static void main(String[] args) {
         int a= 10;
-        int b= 30;
+        int b= 20;
         int c= a+b;
         System.out.println(c);
 
