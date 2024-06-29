2a7d02baad20a020585719ef3bd9a8827bcf839a
tanviruman <ruman777ahmed@gmail.com>
2024-06-30 00:56:57 +0600

    Refactor Bike1 class to improve code structure and readability

--- CT-1/Constructor/src/Bike1.java
+++ CT-1/Constructor/src/Bike1.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Bike1 {
+public class Bike1 {
+
+    Bike1(){
+
+        System.out.println("bike1");
+
+    }
+
+    public static void main(String[] args) {
+        Bike1 b= new Bike1();
+    }
 }
