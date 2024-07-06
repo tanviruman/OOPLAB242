fdb4aa6ba904857cf148cf92c0f15cd99bf368f5
tanviruman <ruman777ahmed@gmail.com>
2024-07-07 01:16:08 +0600

    Refactor emloyee class in LAB-4 module for improved code structure and readability

--- LAB-4/emloyee.java
+++
@@ -1,3 +0,0 @@
-public class emloyee {
-    double sa
-}
---
+++ LAB-4/src/emloyee.java
@@ -0,0 +1,20 @@
+public class emloyee {
+    double salary = 500000;
+}
+
+class programmer extends employee{
+
+    int bonus=100000;
+
+    public static void main(String[] args) {
+
+        
+
+
+
+    }
+
+
+
+
+}
